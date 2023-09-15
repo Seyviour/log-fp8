1b4b3a7a87010870dd90042cd84c212d54438cfd
Saviour <owolabisaviour@gmail.com>
2023-09-15 20:42:24 +0100

    Add documentation

--- rtl/fp8_adder2.v
+++ rtl/fp8_adder2.v
@@ -4,7 +4,18 @@ module adderFP8 #(parameter FP8_TYPE = 1)(
     input wire clk,
     output reg [7:0] C
 );
-
+/*  
+    Input:
+        A[7:0] -> E4M3 Float
+        B[7:0] -> E4M3 Float
+    Output:
+        C[7:0] -> E4M3 Float (sum of A and B inputs)
+        
+    Compute the sum of two E4M3 FP8 numbers, A and B.
+    No support for Nans or Infs.
+    Rounding is towards +Inf or -Inf as necessary
+    Design is purely combinational i.e no delay to result
+*/
 
 wire [3:0] expA, expB;
 wire [2:0] _mantA, _mantB; 
@@ -49,6 +60,15 @@ reg gt;
 
 reg [3:0] expSubArg1, expSubArg2;
 always @(*) begin
+    /*
+    Important to note that in E4M3, exponents of 0000 and 0001
+    are technically the same. They both represent and exponent of -7
+    with `0000` merely indicating that the significand is subnormal.
+    
+    As such, it is beneficial for futher processing to 'normalize' the exponent.
+    We do this here by simply ORing the negation of the reduction OR operation on
+    the exponent
+    */
     expAReg = expA | (!redOrExpA);
     expBReg = expB | (!redOrExpB); 
     sign_diff = signA ^ signB;
@@ -63,8 +83,28 @@ always @(*) begin
     mant2[7:4] = gt? mantB: mantA;
     result_sign = gt? signA: signB;
     exp1 = gt? expAReg: expBReg;
-    // exp_diff_gt_4 = _exp_diff > 4;
     
+    /*
+    This design doesn't use a full-width significand shift/extension.
+    
+    My intuition was that big shift values would ultimately not affect 
+    results significantly, so I thought to take advantage of that to keep
+    the mantissa widths to 8 bits
+
+    And it mostly works, save for the case where a a mantissa of x001 is
+    shifted right by 5 places.
+
+    The deg_check signal detects that case and remedies it by masquerading
+    x001 values as x010 values.
+
+    As this issue only really arises with subtraction, it is handled for that case.
+    
+    See the next always block 
+
+_       mant_sum = mant1 + (sign_diff? (-(mant2S|deg_check)): mant2S);    
+                                              ^^^^^
+*/
+
     {mant2S, deg_check} = {mant2, 1'b0} >> exp_diff; 
 end
 /* 
@@ -76,30 +116,28 @@ in both cases, adding 1 to bit 4 accomplishes rounding
 {5432} is rounded when bit 1 is set
 in both cases, adding 1 to bit 2 accomplishes rounding
 
-{4321} is rounded when bit 0 is set
-{3210} is never rounded? 
-
 */
 
-// 8 7 6 5 4 3 3 2 1 0
-//         x  
 reg [1:0] round; 
 always @(*) begin
     _mant_sum = mant1 + (sign_diff? (-(mant2S|deg_check)): mant2S); 
-    // is_roundable = !({mant2S[1:0], deg_check} == 3'b001); 
+    
+    // rounding value generation
     round[1] = (_mant_sum[8] & _mant_sum[4]) || (_mant_sum[7] & _mant_sum[3]);
     round[0] =  !round[1] && ((_mant_sum[6] & _mant_sum[2]) || ( (_mant_sum[5]) && _mant_sum[1])); 
+
+    // rounding value
     mant_sum = {_mant_sum[8:2] + {round[1], 1'b0, round[0]}, _mant_sum[1:0]}; 
 
 end
 
-reg [1:0] exp_neg;
-reg [2:0] sh_req; 
+reg [1:0] exp_neg;// computer number of places to shift left
+reg [2:0] sh_req; // number of places to shift left. See comment at signal assignment
 
-reg left_shift;
+reg left_shift; // Attempt left shift
 
 always @(*) begin
-    left_shift = !(mant_sum[8] || mant_sum[7]);
+    left_shift = !(mant_sum[8] || mant_sum[7]); 
     exp_neg[1] = (left_shift && !mant_sum[6]) && (mant_sum[5] || mant_sum[4]);
     exp_neg[0] = left_shift && (!mant_sum[5] && mant_sum[4] || mant_sum[6]);
 end
@@ -107,16 +145,37 @@ end
 
 reg [3:0] final_exp;
 
-reg [3:0] true_shift_or_exp;
-reg [4:0] exp_sum_arg;
-reg over_under_flow;
+reg [3:0] true_shift_or_exp; // 
+reg [4:0] exp_sum_arg; // value to add to exponent
+reg over_under_flow; // overflow or underflow
 
 
 always @(*) begin
-    sh_req = {mant_sum==5'b1000, exp_neg}; 
+    /*
+    There is a special case where mant1 = b1000_0000 and mant2S = b0111_1000.
+    The resulting subtration is b0000_1000.
+    As it is the only case where a left shift by 4 is required, we detect it here
+    and set the sh_req as ncessary (exp_neg is guaranteed to be b00 when this occurs, so it's alright)
+    */
+    sh_req = {mant_sum==5'b1000, exp_neg};
+
+    // The logic on the next line inverts (by means of the XOR gate), the value to be shifted
+    // as if to find it's 2's complement (but stopping short of adding '1'). As it turns out,
+    // this \cancels out' in subsequent computations, while makeing the detection of overflows a
+    // lot simpler.
+    // The XOR clears sh_req when no left shift is necessary and OR mant_sum[8] makes exp_sum_arg '1'
+    // for the case where the mantissa sum overflows to the 8th bit
+
     exp_sum_arg = ({2'b00, sh_req} ^ {5{left_shift}}) | mant_sum[8]; 
     {over_under_flow, exp_sum} = exp1 + exp_sum_arg;
-    true_shift_or_exp = exp_sum + (({4{over_under_flow}} & sh_req) | (!over_under_flow));
+
+    // When underflow occurs, the exponent has to be set to 0000, and we need to
+    // readjust the left shift as necessary.
+    // Note that when underflow does not occur, the left shift amount will always be sh_req
+    // when underflow does occur, we must use the value of (exp_sum + 1) as the shift value.
+    // The '+1' compensates for the incomplete two's complement on sh_req. This allows us to
+    // compute the exponent without introducing any new adder!
+    true_shift_or_exp = exp_sum + (over_under_flow?sh_req:1'b1);
 end
 
 reg[3:0] final_mant;
@@ -124,16 +183,20 @@ reg[8:0] shifted_mant;
 
 
 always @(*) begin
+    // shift by true_shift_or exp when underflow occurs, sh_req when no underflow
     shifted_mant = mant_sum << (over_under_flow? true_shift_or_exp: sh_req);
 
-    if (shifted_mant[8]) 
+    if (shifted_mant[8])
+        // mantissa should be set to b1111 when overflow occurs (max_value) 
         final_mant = mant_sum[8:5] | {4{over_under_flow}}; 
     else
         final_mant = shifted_mant[7:4];
 
     if (left_shift) begin
+        // exponent set to 0 when underflow occurs, true_shift_or_exp otherwise
         final_exp = {4{!over_under_flow}} & true_shift_or_exp;
     end else begin
+        // exponent set to b1111 when overflow occurs
         final_exp = {4{over_under_flow}} | exp_sum[3:0];
     end
     
@@ -143,15 +206,6 @@ end
 always @(*) begin
     C = {result_sign, final_exp, final_mant[2:0]}; 
 end
-
-// always @(*) 
-/*
-    Subnormal result is only possible for operands with exponents <= 0011
-    Ergo max exponent difference in that caase is 0011 - 0001 = 0010 (2)
-    
-    maximum result left shift is also 2. Trust me, bro
-*/
-
 
     
 endmodule
